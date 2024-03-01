e39b06bde633701fd824345932a10183ee8a612c
Emanuel23LO <el63050@gmail.com>
2024-02-29 23:32:05 -0500

    amimir

--- nature_project/bookings/views.py
+++ nature_project/bookings/views.py
@@ -16,6 +16,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from django.http import HttpResponse
 
+
 def bookings(request):    
     bookings_list = Booking.objects.all()    
     return render(request, 'bookings/index.html', {'bookings_list': bookings_list})
@@ -154,32 +155,67 @@ def delete_booking(request, booking_id):
     return redirect('bookings')
 
 def edit_booking(request, booking_id):
-    booking = Booking.objects.get(pk=booking_id)
-    customers_list = Customer.objects.all()  # Obtener la lista de clientes
-    cabins_list = Cabin.objects.all()  # Obtener la lista de cabañas
-    services_list = Service.objects.all()  # Obtener la lista de servicios
+    booking = Booking.objects.get(pk=booking_id) 
+    cabins = Cabin.objects.filter(booking_cabin__booking=booking)
+    services = Service.objects.filter(booking_service__booking=booking)
 
+    customers_list = Customer.objects.all()
+    cabins_list = Cabin.objects.all()
+    services_list = Service.objects.all()    
+    
     if request.method == 'POST':
-        # Actualizar los campos de la reserva con los datos enviados en la solicitud POST
-        try:
-            date_start_str = request.POST['date_start']
-            date_end_str = request.POST['date_end']        
-            date_start = datetime.strptime(date_start_str, '%Y-%m-%d')
-            date_end = datetime.strptime(date_end_str, '%Y-%m-%d')
-            
-            booking.date_start = date_start
-            booking.date_end = date_end
-            booking.value = request.POST['totalValue']
-            booking.status = 'Reservado'  # Siempre lo establecemos a 'Reservado' en esta vista
-            
-            # Guardar los cambios en la reserva
-            booking.save()
-            
-            messages.success(request, 'Reserva actualizada correctamente.')  # Mensaje de éxito
-            return redirect('bookings')  # Redirigir a la página de listado de reservas después de la actualización
-        except Exception as e:
-            messages.error(request, f'Ocurrió un error al editar la reserva: {e}')  # Mensaje de error
-            return redirect('edit_booking', booking_id=booking_id)
+        date_str = request.POST.get('date_start', '')
+        date_end_str = request.POST.get('date_end', '')        
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+        date_end = datetime.strptime(date_end_str, '%Y-%m-%d')
+
+        # Print después de asignar valores
+        print("Date Start:", date_str)
+        print("Date End:", date_end_str)
+        print("Total Value:", request.POST.get('totalValue', ''))
+        print("Customer ID:", request.POST.get('customer', ''))
+
+        # Actualizar los campos del objeto booking con los valores recibidos del formulario
+        booking.date_start = date
+        booking.date_end = date_end
+        booking.value = request.POST.get('totalValue', '')
+        booking.customer_id = request.POST.get('customer', '')
         
-    # Si la solicitud no es POST, renderiza la página de edición con los datos de la reserva
-    return render(request, 'bookings/create.html', {'booking': booking, 'customers_list': customers_list, 'cabins_list': cabins_list, 'services_list': services_list})
+        # Guardar los cambios en la base de datos      
+
+        # Actualizar los objetos relacionales Cabin y Service
+        booking_cabins = Booking_cabin.objects.filter(booking=booking)
+        booking_services = Booking_service.objects.filter(booking=booking)
+        payments = Payment.objects.filter(booking=booking)
+            
+        # Eliminar los objetos Cabin y Service existentes asociados a esta reserva
+        booking_cabins.delete()
+        booking_services.delete()
+
+        # Crear nuevos objetos Cabin y Service con los valores actualizados
+        for cabin_id in request.POST.getlist('cabinId[]'):
+            cabin = Cabin.objects.get(pk=cabin_id)
+            cabin_value = request.POST.get(f'cabinValue[{cabin_id}]', '')
+            booking_cabin = Booking_cabin.objects.create(
+                booking=booking,
+                cabin=cabin,
+                value=cabin_value
+            )
+            booking_cabin.save()
+
+        for service_id in request.POST.getlist('serviceId[]'):
+            service = Service.objects.get(pk=service_id)
+            service_value = request.POST.get(f'serviceValue[{service_id}]', '')
+            booking_service = Booking_service.objects.create(
+                booking=booking,
+                service=service,
+                value=service_value
+            )
+            booking_service.save()
+              
+        # Redireccionar a la página de listado de reservas con un mensaje de éxito
+        messages.success(request, 'Reserva editada con éxito.')
+        return redirect('bookings')
+    
+    # Si la solicitud es GET, renderizar el formulario de edición con los datos del objeto booking
+    return render(request, 'bookings/edit.html', {'booking': booking, 'customers_list': customers_list, 'cabins_list': cabins_list, 'services_list': services_list, 'cabins': cabins, 'services': services })
